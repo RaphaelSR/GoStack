85e731ac210b89bcaac958f10680f11c7f071b9d
Raphael Souza Rocha <raphaelrochabcc@gmail.com>
2020-10-19 00:01:13 -0300

    dashboard and repo css ok

--- 05-primeiro-projeto-react/src/pages/Repository/index.tsx
+++ 05-primeiro-projeto-react/src/pages/Repository/index.tsx
@@ -1,7 +1,63 @@
 import React from 'react';
+import {useRouteMatch, Link} from 'react-router-dom';
+import {FiChevronLeft, FiChevronRight} from 'react-icons/fi';
+import logoImg from '../../assets/logo.svg';
+import {Header, RepositoryInfo, Issues} from './styles';
+
+interface RepositoryParams {
+    repository: string;
+}
 
 const Repository: React.FC = () => {
-    return <h1>Repository</h1>;
+    const {params} = useRouteMatch<RepositoryParams>();
+    return (
+        <>
+        <Header>
+            <img src={logoImg} alt="Github Explorer"/>
+            <Link to="/dashboard">
+                <FiChevronLeft size={16}/>
+            Voltar
+            </Link>
+        </Header>
+
+
+        <RepositoryInfo>
+            <header>
+                <img src="https://avatars0.githubusercontent.com/u/28929274?v=4" alt="Rocketseat"/>
+                <div>
+                    <strong>rocketseat/unform</strong>
+                    <p>Descrição do repositório</p>                    
+                </div>
+                </header>
+                <ul>
+                <li>
+                    <strong>1808</strong>
+                    <span>Stars</span>
+                </li>
+                <li>
+                    <strong>1808</strong>
+                    <span>Forks</span>
+                </li>
+                <li>
+                    <strong>1808</strong>
+                    <span>Issues abertas</span>
+                </li>
+            </ul>
+        </RepositoryInfo>
+
+        <Issues>
+        <Link to={`aaa`}>
+                 <div>
+                     <strong>aa</strong>
+                     <p>aa</p>
+                 </div>
+                
+                <FiChevronRight size={20} />
+        </Link>
+        </Issues>
+        
+        </>
+    );
 };
 
 export default Repository;
