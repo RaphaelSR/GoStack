a7bca16448f3a23eb5185dcab0143addfdab6998
Raphael Souza Rocha <raphaelrochabcc@gmail.com>
2020-12-18 15:37:17 -0300

    Recuperação de senha

--- iniciando-back-end/src/modules/users/services/AuthenticateUserService.spec.ts
+++ iniciando-back-end/src/modules/users/services/AuthenticateUserService.spec.ts
@@ -44,7 +44,7 @@ describe('AuthenticateUser', () => {
       fakeHashProvider,
     );
 
-    expect(
+    await expect(
       authenticateUser.execute({
         email: 'johndoe@example.com',
         password: '123123',
