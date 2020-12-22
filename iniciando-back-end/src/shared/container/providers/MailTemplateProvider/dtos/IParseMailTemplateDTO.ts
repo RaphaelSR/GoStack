7e86d8f6871ecbbb2003e8eac5d1abf0116de8f9
Raphael Souza Rocha <raphaelrochabcc@gmail.com>
2020-12-22 15:45:55 -0300

    Template engine

--- iniciando-back-end/src/shared/container/providers/MailTemplateProvider/dtos/IParseMailTemplateDTO.ts
+++ iniciando-back-end/src/shared/container/providers/MailTemplateProvider/dtos/IParseMailTemplateDTO.ts
@@ -1,9 +1,8 @@
-
 interface ITemplateVariables {
     [key: string]: string | number;
 }
 
 export default interface IParseMailTemplateDTO {
-    template: string;
+    file: string;
     variables: ITemplateVariables;
 }
