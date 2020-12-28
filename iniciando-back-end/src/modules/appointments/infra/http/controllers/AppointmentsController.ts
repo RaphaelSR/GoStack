63656c7df73fa0c28d20766718deafcc4a8c4147
Raphael Souza Rocha <raphaelrochabcc@gmail.com>
2020-12-28 15:07:45 -0300

    Criação do agendamento

--- iniciando-back-end/src/modules/appointments/infra/http/controllers/AppointmentsController.ts
+++ iniciando-back-end/src/modules/appointments/infra/http/controllers/AppointmentsController.ts
@@ -1,22 +1,25 @@
 import { Request, Response } from 'express';
-import { parseISO } from 'date-fns';
 import { container } from 'tsyringe';
-
+import { parseISO } from 'date-fns';
 import CreateAppointmentService from '@modules/appointments/services/CreateAppointmentService';
 
 export default class AppointmentsController {
-  public async create(request: Request, response: Response): Promise<Response> {
-    const { provider_id, date } = request.body;
+    public async create(
+        request: Request,
+        response: Response,
+    ): Promise<Response> {
+        const user_id = request.user.id;
+        const { provider_id, date } = request.body;
 
-    const parsedDate = parseISO(date);
+        const parsedDate = parseISO(date);
 
-    const createAppointment = container.resolve(CreateAppointmentService);
+        const createAppointment = container.resolve(CreateAppointmentService);
 
-    const appointment = await createAppointment.execute({
-      date: parsedDate,
-      provider_id,
-    });
-
-    return response.json(appointment);
-  }
+        const appointment = await createAppointment.execute({
+            date: parsedDate,
+            provider_id,
+            user_id,
+        });
+        return response.json(appointment);
+    }
 }
--- iniciando-back-end/src/modules/appointments/services/CreateAppointmentService.ts
+++ iniciando-back-end/src/modules/appointments/services/CreateAppointmentService.ts
@@ -1,41 +1,48 @@
-import { startOfHour } from 'date-fns';
-import { injectable, inject } from 'tsyringe';
-
 import AppError from '@shared/errors/AppError';
+import { startOfHour } from 'date-fns';
+
+import { injectable, inject } from 'tsyringe';
 
 import Appointment from '../infra/typeorm/entities/Appointment';
 import IAppointmentsRepository from '../repositories/IAppointmentsRepository';
 
 interface IRequest {
-  date: Date;
-  provider_id: string;
+    provider_id: string;
+    date: Date;
+    user_id: string;
 }
 
 @injectable()
 class CreateAppointmentService {
-  constructor(
-    @inject('AppointmentsRepository')
-    private appointmentsRepository: IAppointmentsRepository,
-  ) {}
-
-  public async execute({ date, provider_id }: IRequest): Promise<Appointment> {
-    const appointmentDate = startOfHour(date);
-
-    const findAppointmentInSameDate = await this.appointmentsRepository.findByDate(
-      appointmentDate,
-    );
-
-    if (findAppointmentInSameDate) {
-      throw new AppError('This appointment is already booked');
+    constructor(
+        @inject('AppointmentsRepository')
+        private appointmentsRepository: IAppointmentsRepository,
+    ) {
+        this.appointmentsRepository = appointmentsRepository;
     }
 
-    const appointment = await this.appointmentsRepository.create({
-      provider_id,
-      date: appointmentDate,
-    });
+    public async execute({
+        date,
+        provider_id,
+        user_id,
+    }: IRequest): Promise<Appointment> {
+        const appointmentDate = startOfHour(date);
 
-    return appointment;
-  }
+        const findAppointamentSameDate = await this.appointmentsRepository.findByDate(
+            appointmentDate,
+        );
+
+        if (findAppointamentSameDate) {
+            throw new AppError('This appointment is already booked');
+        }
+
+        const appointment = await this.appointmentsRepository.create({
+            provider_id,
+            user_id,
+            date: appointmentDate,
+        });
+        return appointment;
+    }
 }
 
 export default CreateAppointmentService;
