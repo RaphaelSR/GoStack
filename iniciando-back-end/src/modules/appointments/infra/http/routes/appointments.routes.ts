e06135e9b5c764d6332b462fc76ee6520f265c4c
Raphael Souza Rocha <raphaelrochabcc@gmail.com>
2020-12-29 10:47:24 -0300

    Agenda do prestador

--- iniciando-back-end/src/modules/appointments/infra/http/routes/appointments.routes.ts
+++ iniciando-back-end/src/modules/appointments/infra/http/routes/appointments.routes.ts
@@ -1,13 +1,22 @@
-import { Router } from 'express';
-
 import ensureAuthenticated from '@modules/users/infra/http/middlewares/ensureAuthenticated';
+
+import { Router } from 'express';
 import AppointmentsController from '../controllers/AppointmentsController';
+import ProviderAppointmentsController from '../controllers/ProviderAppointmentsController';
 
 const appointmentsRouter = Router();
 const appointmentsController = new AppointmentsController();
+const providerAppointmentsController = new ProviderAppointmentsController();
 
 appointmentsRouter.use(ensureAuthenticated);
 
+// appointmentsRouter.get('/', async (request, response) => {
+//     const appoitments = await appointmentsRepository.find();
+
+//     return response.json(appoitments);
+// });
+
 appointmentsRouter.post('/', appointmentsController.create);
+appointmentsRouter.get('/me', providerAppointmentsController.index);
 
 export default appointmentsRouter;
