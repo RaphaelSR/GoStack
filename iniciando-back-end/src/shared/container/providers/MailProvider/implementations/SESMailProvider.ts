8d48a035dfee07d9b51c45b5ca76267cb420f38b
Raphael Souza Rocha <raphaelrochabcc@gmail.com>
2021-01-12 16:30:35 -0300

    12 min Not finished Emails pelo Amazon SES

--- iniciando-back-end/src/shared/container/providers/MailProvider/implementations/SESMailProvider.ts
+++ iniciando-back-end/src/shared/container/providers/MailProvider/implementations/SESMailProvider.ts
@@ -9,39 +9,39 @@ import ISendMailDTO from '../dtos/ISendMailDTO';
 
 @injectable()
 export default class SESMailProvider implements IMailProvider {
-  private client: Transporter;
+    private client: Transporter;
 
-  constructor(
-    @inject('MailTemplateProvider')
-    private mailTemplateProvider: IMailTemplateProvider,
-  ) {
-    this.client = nodemailer.createTransport({
-      SES: new aws.SES({
-        apiVersion: '2010-12-01',
-        region: 'us-east-1',
-      }),
-    });
-  }
+    constructor(
+        @inject('MailTemplateProvider')
+        private mailTemplateProvider: IMailTemplateProvider,
+    ) {
+        this.client = nodemailer.createTransport({
+            SES: new aws.SES({
+                apiVersion: '2010-12-01',
+                region: 'us-east-1',
+            }),
+        });
+    }
 
-  public async sendMail({
-    to,
-    from,
-    subject,
-    templateData,
-  }: ISendMailDTO): Promise<void> {
-    const { name, email } = mailConfig.defaults.from;
+    public async sendMail({
+        to,
+        from,
+        subject,
+        templateData,
+    }: ISendMailDTO): Promise<void> {
+        const { name, email } = mailConfig.defaults.from;
 
-    await this.client.sendMail({
-      from: {
-        name: from?.name || name,
-        address: from?.email || email,
-      },
-      to: {
-        name: to.name,
-        address: to.email,
-      },
-      subject,
-      html: await this.mailTemplateProvider.parse(templateData),
-    });
-  }
+        await this.client.sendMail({
+            from: {
+                name: from?.name || name,
+                address: from?.email || email,
+            },
+            to: {
+                name: to.name,
+                address: to.email,
+            },
+            subject,
+            html: await this.mailTemplateProvider.parse(templateData),
+        });
+    }
 }
